0fede9ba6939c17873cc4e2ec474aa1b4e6e0891
massi <massimo.battinelli@gmail.com>
2022-03-12 12:29:50 +0100

    fine progetto

---
+++ main.js
@@ -0,0 +1,60 @@
+var root = new Vue({
+    el: '#root',
+    data: {
+      timer: null,
+      currentSlide: 0,
+      slidesArray: [
+
+        {
+              title:'Svezia',
+              text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+              items:'img/01.jpg'
+        },
+
+        {
+            title:'Svizzera',
+            text:'Lorem ipsum',
+            items:'img/02.jpg'
+        },
+
+        {
+            title:'Gran Bretagna',
+            text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            items:'img/03.jpg'
+        },
+
+        {
+            title:'Germania',
+            text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            items:'img/04.jpg'
+        },
+
+        {
+            title:'Paradise',
+            text:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            items:'img/05.jpg'
+        }
+
+      ]
+
+    },
+
+    methods:{
+        next: function(){
+            this.currentSlide < this.slidesArray.length - 1 ? this.currentSlide++ : this.currentSlide = 0;
+        },
+        prev: function(){
+            this.currentSlide > 0 ? this.currentSlide-- : this.currentSlide = this.slidesArray.length - 1;
+        },
+        startTimer: function(){
+            this.timer = setInterval(this.next,3000);
+        },
+        stopTimer: function(){
+            clearInterval(this.timer);
+        }
+    },
+    created(){
+        this.startTimer();
+    }
+
+  })
